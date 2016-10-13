e6cf5a7d5e36b6ab5e54c94428ccf83beab048ab
Yosua1011 <Yosua1011@gmail.com>
2016-10-13 10:35:17 +0700

    Style : change style of from ... import ...

--- send_text.py
+++ send_text.py
@@ -1,9 +1,9 @@
-from twilio.rest import TwilioRestClient
+from twilio import rest
 
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "ACb0980dde16552bc6988e3567fda81878"
 auth_token = "fe8bfaea54fd9ea1cb860372475d098f"
-client = TwilioRestClient(account_sid, auth_token)
+client = rest.TwilioRestClient(account_sid, auth_token)
 
 message = client.sms.messages.create(
 	body="Hello, World!",
